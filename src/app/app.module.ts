a935eb948e20839c18b696c4f5887e46fa2785af
Bolorchimeg <bolorchimeg.g@gmail.com>
2021-12-23 07:58:33 +1100

    add task part has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -8,6 +9,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  text: string="";
+  day: string="";
+  reminder:boolean=false;
+  @Output() onAddTask:EventEmitter<Task>=new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    if(!this.text){
+      alert("Please add text");
+      this.text="";
+      return;
+    }
+    
+    
+    const newTask = {
+        text: this.text,
+        day:this.day,
+        reminder:this.reminder
+      }
+    this.text="";
+    this.day="";
+    this.reminder=false;
+    
+    this.onAddTask.emit(newTask);
+  }
+  
 }
