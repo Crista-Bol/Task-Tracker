fd7102ae4d3a6ff85289a098633ac36d1e242158
Bolorchimeg <bolorchimeg.g@gmail.com>
2021-12-23 07:16:25 +1100

    task delete and update parts have been added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,4 +13,8 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleTaskBar(){
+    console.log('btn clicked');
+  }
+ 
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import { Task } from "./Task"
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Barbers appointment',
+        day: 'Aug 5th at 3:30pm',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'Dentists appointment',
+        day: 'Jan 5th at 2:30pm',
+        reminder: true
+    }
+]
